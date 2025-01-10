84faac79978054b357c1041109667fefd184a024
Shaheera <vshaheera89@gmail.com>
2025-01-10 10:28:12 +0530

    added search box

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 import Header from "@/components/Header";
 import Navbar from "@/components/Navbar";
 import Providers from "./Providers";
+import SearchBox from "@/components/SearchBox";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,6 +29,7 @@ export default function RootLayout({ children }) {
         <Providers>
           <Header />
           <Navbar />
+          <SearchBox />
           {children}
         </Providers>
       </body>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,7 +7,7 @@ const Card = ({result}) => {
   return (
     <div className='group cursor-pointer sm:hover:shadow-slate-400 sm:shadow-md rounded-lg sm:border sm:border-slate-400 sm:m-2 transition-shadow duration-200'>
       <Link href={`/movie/${result.id}`}>
-        <Image src={`https://image.tmdb.org/t/p/original/${result.backdrop_path || item.poster_path}`}
+        <Image src={`https://image.tmdb.org/t/p/original/${result.backdrop_path || result.poster_path}`}
         width={500}
         height={300}
         className='sm:rounded-t-lg group-hover:opacity-75 transition-opacity duration-300' >
